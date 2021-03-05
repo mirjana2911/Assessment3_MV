b055c00a02cf809bf6e05a7b5d5a37e371cdec0a
mirjana2911 <mirjana.dragonfly@gmail.com>
2021-03-05 20:49:59 +0000

    Update8

--- script.py
+++ script.py
@@ -165,3 +165,43 @@ sec_write = LocalUpdateSections(courseid, data)
 sec = LocalGetSections(courseid)
 print(json.dumps(sec.getsections[1]['summary'], indent=4, sort_keys=True))
 
+####################################################################### Recording
+
+import requests
+
+def download_file_from_google_drive(id, destination):
+    URL1 = "https://drive.google.com/file/d/1wg0gkGZp19JJdTa8Zim3L1_jCH4vawa1"
+
+    session = requests.Session()
+
+    response = session.get(URL1, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL1, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    file_id = 'https://drive.google.com/file/d/1wg0gkGZp19JJdTa8Zim3L1_jCH4vawa1'
+    destination = 'VIDEO.html'
+    download_file_from_google_drive(file_id, destination)
+  
+    
+  
